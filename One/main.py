67a0642d94024a21988beb03d89dd274114f0a87
Jack Martinka <91498376+Venkal@users.noreply.github.com>
2024-02-21 22:35:58 -0600

    Aded a loop through the images folder.
    Currently the second image takes a while to show

--- One/main.py
+++ One/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from PIL import Image
 
 from Guassian_Blur import gaussian_blur2
@@ -8,15 +9,31 @@ from Non_Maximum_Suppression import non_maximum_suppression
 from Thresholding import threshold
 
 if __name__ == '__main__':
-    img = Image.open('.\images\GrenoraCropped.jpg') 
+    srcD = '.\images\\'
+    for file in os.scandir(srcD):
+        filename = file.path
+        print(filename)
+        img = Image.open(filename)
+        gray = np.array(img.convert('L'))
+        blur = gaussian_blur2(gray, 1, (40, 40))
+        # Might need to change sigma to 1, the image might still be too blurry
+        gradient, theta = sobel_filter(blur, 1.5, (10, 10))
+        nms = non_maximum_suppression(gradient, theta)
+        th = threshold(nms, 0.05, 0.09)
+        print('blurs fault')
+        plt.figure()
+        plt.imshow(nms, cmap='gray')
+        plt.show(block=True)
+        
+    #img = Image.open('.\images\GrenoraCropped.jpg')
     # now relative address, still need to find way to not hard-code what image
-    gray = np.array(img.convert('L'))
-    blur = gaussian_blur2(gray, 1, (40, 40))
+    #gray = np.array(img.convert('L'))
+    #blur = gaussian_blur2(gray, 1, (40, 40))
     # Might need to change sigma to 1, the image might still be too blurry
-    gradient, theta = sobel_filter(blur, 1.5, (10, 10))
-    nms = non_maximum_suppression(gradient, theta)
-    th = threshold(nms, 0.05, 0.09)
+    #gradient, theta = sobel_filter(blur, 1.5, (10, 10))
+    #nms = non_maximum_suppression(gradient, theta)
+    #th = threshold(nms, 0.05, 0.09)
 
-    plt.figure()
-    plt.imshow(nms, cmap='gray')
-    plt.show()
+    #plt.figure()
+    #plt.imshow(nms, cmap='gray')
+    #plt.show(True)

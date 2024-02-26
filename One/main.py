a9a0df18d8a1509fc20f2fd44a5534e84de3ca2e
Lucia Potter <lapotter2000@gmail.com>
2024-02-26 11:18:49 -0600

    output producing from certain images

--- One/main.py
+++ One/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import cv2
+import pytesseract
 
 from Guassian_Blur import gaussian_blur2
 from Sobel_Filter import sobel_filter
@@ -8,7 +10,8 @@ from Non_Maximum_Suppression import non_maximum_suppression
 from Thresholding import threshold
 
 if __name__ == '__main__':
-    img = Image.open('.\images\GrenoraCropped.jpg') 
+    pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+    img = Image.open('./images/RayCropped.jpg') 
     # now relative address, still need to find way to not hard-code what image
     gray = np.array(img.convert('L'))
     blur = gaussian_blur2(gray, 1, (40, 40))
@@ -17,6 +20,15 @@ if __name__ == '__main__':
     nms = non_maximum_suppression(gradient, theta)
     th = threshold(nms, 0.05, 0.09)
 
-    plt.figure()
-    plt.imshow(nms, cmap='gray')
-    plt.show()
+    # i commented out some stuff just to test various things
+    #plt.figure()
+    #plt.imshow(nms, cmap='gray')
+    #plt.show()
+
+    #pytesseract below?
+    data = pytesseract.image_to_string(img, lang='eng',config='--psm 6')
+    print(data)
+
+    #img2 = cv2.imread('./images/GrenoraCropped.jpg')
+    #text = pytesseract.image_to_string(img2)
+    #print(text)

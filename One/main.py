939ed1bfcf50112bf3bb3810782c6d545385a78c
fredhigh <fred@FREDSPC>
2024-02-26 15:00:04 -0600

    Added in cropping for preprocessing

--- One/main.py
+++ One/main.py
@@ -14,16 +14,25 @@ if __name__ == '__main__':
         filename = file.path
         print(filename)
         img = Image.open(filename)
-        gray = np.array(img.convert('L'))
-        blur = gaussian_blur2(gray, 1, (40, 40))
+        width, height = img.size
+        print(str(width) + " " + str(height) + " " + str(img.size))
+        left = width * .25
+        top = 0
+        right = width * .75
+        bottom = height
+        cmg = img.crop((left, top, right, bottom))
+        #gray = np.array(cmg.convert('L'))
+        #blur = gaussian_blur2(gray, 1, (40, 40))
         # Might need to change sigma to 1, the image might still be too blurry
-        gradient, theta = sobel_filter(blur, 1.5, (10, 10))
-        nms = non_maximum_suppression(gradient, theta)
-        th = threshold(nms, 0.05, 0.09)
-        print('blurs fault')
-        plt.figure()
-        plt.imshow(nms, cmap='gray')
+        #gradient, theta = sobel_filter(blur, 1.5, (10, 10))
+        #nms = non_maximum_suppression(gradient, theta)
+        #th = threshold(nms, 0.05, 0.09)
+
+        plt.imshow(cmg)
         plt.show(block=True)
+        #plt.figure()
+        #plt.imshow(nms, cmap='gray')
+        #plt.show(block=True)
         
     #img = Image.open('.\images\GrenoraCropped.jpg')
     # now relative address, still need to find way to not hard-code what image

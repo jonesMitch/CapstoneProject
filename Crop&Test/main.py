39f52c81697c1907ebe7a1e2f11f9fe1669aa70c
Lucia Potter <lapotter2000@gmail.com>
2024-03-05 21:45:13 -0600

    removing non-numeric characters

--- Crop&Test/main.py
+++ Crop&Test/main.py
@@ -104,7 +104,9 @@ if __name__ == '__main__':
         #cv2.waitKey(0)
         #cv2.destroyAllWindows()
         data = pytesseract.image_to_string(mask, lang='eng',config='--psm 6')
-        print(data)
+        nums = data.lower().translate({ord(i): None for i in 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_+=/:;".,<>?—~|\[]{} \''})
+        #stupid hardcoding here to remove all non-number characters from string
+        print(nums)
         
     #img = Image.open('.\images\GrenoraCropped.jpg')
     # now relative address, still need to find way to not hard-code what image

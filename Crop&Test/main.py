356d2e59511dd1ca9a703252b0c8445796bdd48f
Lucia Potter <lapotter2000@gmail.com>
2024-03-18 19:51:46 -0500

    crop/test has latest stuff - main dot py for demo

--- Crop&Test/main.py
+++ Crop&Test/main.py
@@ -122,10 +122,10 @@ if __name__ == '__main__':
         controlList = [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
         numList = []
         #stupid hardcoding here to remove all non-number characters from string
-        for number in nums:
-            intNum = int(number)
-            numList.append(intNum)
-        print(numList)
+        #for number in nums:
+        #    intNum = int(number)
+        #    numList.append(intNum)
+        print(nums) #replace with functional list when working
         #depth = min(numList)
         #print(depth)
         

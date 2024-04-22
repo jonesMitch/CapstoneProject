2ddd30853664a7ee3366f30cff1ac52aa4ff5e14
fredhigh <fred@FREDSPC>
2024-04-22 18:41:09 -0500

    Fixing merging errors.

--- One/main.py
+++ One/main.py
@@ -159,16 +159,21 @@ if __name__ == '__main__':
                 "needs review": False
             }
 
+        #Output
+        file = open(".\JSONs\\" + nameJson + ".json", "w")
+        json.dump(data, file, indent=3)
+        file.close()
+
 if __name__ == '__main__':
     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-    img = Image.open('./images/testimage.jpg') 
+    #img = Image.open('./images/testimage.jpg') 
     # now relative address, still need to find way to not hard-code what image
-    gray = np.array(img.convert('L'))
-    blur = gaussian_blur2(gray, 1, (40, 40))
+    #gray = np.array(img.convert('L'))
+    #blur = gaussian_blur2(gray, 1, (40, 40))
     # Might need to change sigma to 1, the image might still be too blurry
-    gradient, theta = sobel_filter(blur, 1.5, (10, 10))
-    nms = non_maximum_suppression(gradient, theta)
-    th = threshold(nms, 0.05, 0.09)
+    #gradient, theta = sobel_filter(blur, 1.5, (10, 10))
+    #nms = non_maximum_suppression(gradient, theta)
+    #th = threshold(nms, 0.05, 0.09)
 
     # i commented out some stuff just to test various things
     #plt.figure()
@@ -176,15 +181,11 @@ if __name__ == '__main__':
     #plt.show()
 
     #pytesseract below?
-    data = pytesseract.image_to_string(img, lang='eng',config='--psm 6')
-    print(data)
+    #data = pytesseract.image_to_string(img, lang='eng',config='--psm 6')
+    #print(data)
 
     #img2 = cv2.imread('./images/GrenoraCropped.jpg')
     #text = pytesseract.image_to_string(img2)
     #print(text)
 
-    #Output
-    file = open(".\JSONs\\" + nameJson + ".json", "w")
-    json.dump(data, file, indent=3)
-    file.close()
-main
+    
